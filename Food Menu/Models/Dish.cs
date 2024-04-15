6c136ac8750965861b456a79ebdb24d375acc022
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-04-15 17:58:40 +0200

    Initial commit: Setup project structure and dependencies

---
+++ Food Menu/Models/Dish.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Food_Menu.Models
+{
+	public class Dish
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string ImageUrl { get; set; }
+		public double Price { get; set; }
+
+        public List<DishIngredient>? DishIngredients { get; set; }
+    }
+}
+
---
+++ Food Menu/Models/Ingredient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Food_Menu.Models
+{
+	public class Ingredient
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public List<DishIngredient>? DishIngredients { get; set; }
+	}
+}
+
