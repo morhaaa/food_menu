49c1d4cca28bcc51ca6f8f89de08397e115f6309
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-04-16 16:35:27 +0200

    New(Controller): create menu controller

---
+++ Food Menu/Controllers/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Food_Menu.Data;
+using Food_Menu.Models;
+
+namespace Food_Menu.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly MenuContext _context;
+        
+        public MenuController(MenuContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Dishes.ToListAsync());
+        }
+    }
+}
+
